05fe0865a4a6a5a889e35871855a051bd206b979
dhiraj92 <dhiraj92@gmail.com>
2016-04-04 22:07:47 -0700

    getting detailed reviews

--- itemlookup.py
+++ itemlookup.py
@@ -16,63 +16,70 @@ tag = accesslist[2].strip("\n")
 amzn = AmazonScraper(keyid,accesskey,tag)
 api = amazonproduct.API(locale ='us',access_key_id=keyid,secret_access_key = accesskey,associate_tag = tag)
 
-def price_offers(asin):
-    str_asin = str(asin)
-    try:
-        node = api.item_lookup(ItemId=str_asin, ResponseGroup='Offers', Condition='All', MerchantId='All')        
-        for a in node.Items.Item.Offers.Offer:
-            price = a.OfferListing.Price.FormattedPrice
-            print a.OfferListing.Price.FormattedPrice
-    except Exception:
-        print "not found"
-        return "NaN"
-    #return price
-    str_asin = str(asin)
-
-def sales_rank(asin):
-    str_asin = str(asin)
-    try:
-        node = api.item_lookup(ItemId=str_asin, ResponseGroup='SalesRank', Condition='All', MerchantId='All')
-        
-        for a in node.Items.Item.SalesRank:
-            rank = a
-            print "rank is", rank
-            
-            #print a.OfferListing.Price.FormattedPrice
-    except Exception:
-        print "not found"
-        return "NaN"
+##def price_offers(asin):
+##    str_asin = str(asin)
+##    try:
+##        node = api.item_lookup(ItemId=str_asin, ResponseGroup='Offers', Condition='All', MerchantId='All')        
+##        for a in node.Items.Item.Offers.Offer:
+##            price = a.OfferListing.Price.FormattedPrice
+##            print a.OfferListing.Price.FormattedPrice
+##    except Exception:
+##        print "not found"
+##        return "NaN"
+##    #return price
+##    str_asin = str(asin)
+##
+##def sales_rank(asin):
+##    str_asin = str(asin)
+##    try:
+##        node = api.item_lookup(ItemId=str_asin, ResponseGroup='SalesRank', Condition='All', MerchantId='All')
+##        
+##        for a in node.Items.Item.SalesRank:
+##            rank = a
+##            print "rank is", rank
+##            
+##            #print a.OfferListing.Price.FormattedPrice
+##    except Exception:
+##        print "not found"
+##        return "NaN"
     #return price
 def large_data(asin):
     str_asin = str(asin)
     try:
         node = api.item_lookup(ItemId=str_asin, ResponseGroup='Large', Condition='All', MerchantId='All')
-        print node
         rs = amzn.reviews(ItemId=str_asin)
         r = amzn.review(Id=rs.ids[0])
         reviewRating = r.rating
         
-        pdb.set_trace()
+        #Default ranking for all interviews is by date
+        for r in rs.full_reviews():
+            print r.id
+            print r.rating
+            print r.text
+            print r.title
+            print r.date
+            print r.user
+            pdb.set_trace()
+            break       
          
         print "releasedate is", node.Items.Item.ItemAttributes.ReleaseDate
         print "sales rank is", node.Items.Item.SalesRank
         #it is full html style, html can be cleaned using beautifulsoup
         #print "product description is", node.Items.Item.EditorialReviews.EditorialReview.Content
-        
-        for a in node.Items.Item.SalesRank:
-            price = a
-            print "large data is", price            
-            #print a.OfferListing.Price.FormattedPrice
+        for a in node.Items.Item.Offers.Offer:
+            price = a.OfferListing.Price.FormattedPrice
+            print "Price best offer is " , a.OfferListing.Price.FormattedPrice
+
     except Exception:
         print "exception not found"
         return "NaN"
     return price    
 
 def main():
-    print price_offers("B01637RFR4")
-    print sales_rank("B01637RFR4")
+    #print price_offers("B01637RFR4")
+    #print sales_rank("B01637RFR4")
     print large_data("B01637RFR4")
-    print price_offers("B00UB76290")
+    #print price_offers("B00UB76290")
     #print large_data("B01637RFR4")
                    
             
